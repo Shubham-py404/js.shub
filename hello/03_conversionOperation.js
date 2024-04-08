63a3411584cb29ed7a7e9e553ff404c3b6973bd8
Shubham Bansal <159136423+Shubham-py404@users.noreply.github.com>
2024-04-08 15:36:25 +0000

    progress

---
+++ hello/03_conversionOperation.js
@@ -0,0 +1,60 @@
+// let score = undefined
+
+// console.log(typeof score) //number
+// console.log(typeof (score))//same 
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//conversions
+// '33'=>33
+//'48asdf' => NaN (not a no.)
+// true => 1; false => 0 
+
+// let isLoggedIn = ''  //if empty false or if no. or string it will be true 
+ 
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+///result
+
+// 1=> true; 0 =>false 
+// "" => false 
+// "shub" => true 
+
+// let sumNumber = 33
+// let stringNumber = String(sumNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+ 
+//***********************operations************** 
+
+// let value = 3
+// let negValue= -value
+// console.log(negValue);
+
+// let str1 = 'hello'
+// let str2 = ' shubham'
+// let str3 = str1 + str2 //string concatenation
+// console.log(str3);
+ 
+
+// console.log("1"+2); //12
+// console.log(1+'2');//12
+// console.log('1' + 2+2);//122
+// console.log(1+'2'+2);//122
+// console.log(1+2+'2');//32
+
+// console.log((3+4)*5%3 );
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1,num2,num3
+num1=num2=num=2+2
+let gameCounter=100
+// gameCounter++; //101
+++gameCounter;
+console.log(gameCounter)
