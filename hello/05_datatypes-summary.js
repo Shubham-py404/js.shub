5c19be6d16736a984dca6920ab89b6d481f8e7f1
Shubham Bansal <159136423+Shubham-py404@users.noreply.github.com>
2024-04-13 15:40:12 +0000

    done

---
+++ hello/05_datatypes-summary.js
@@ -0,0 +1,53 @@
+// primitive 
+
+// 7 types : String,Number ,null ,undefined, boolean,symbol,BigInt
+
+
+
+
+const score = 100
+const scorevalue= 100.3
+const isloggedIn = false 
+const outsideTemp = undefined
+const isName ='Gojo satoru'
+let userEmail; //value will undefined 
+
+console.log(isName);
+
+const id = Symbol('123') 
+//hamesha unique hona chahiye data type - symbol
+
+// const anotherid = Symbol('123')
+// console.log(id==anotherid); // false 
+
+// const bigNumber =  354524254242n
+
+// reference (non  primitive ) 
+ 
+// Array, Objects, Functions
+  
+const hero = ['megumi','nobara','maki'] // array like list 
+let myobj ={
+    name:'gojo',    
+    age : 22,
+}
+//like dict 
+
+//funtions  type - function 
+
+const myfunction = function(){
+      console.log('hell world')
+}
+ 
+myfunction()
+
+// console.log(typeof bigNumber)
+
+
+//type val     Result
+/*
+undefined    'undefinded'
+null          'object '
+boolean        'boolean'
+number         'number '
+string          'string' */
